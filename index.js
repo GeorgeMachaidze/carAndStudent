6fdbfbdbbad42e235f077e4656b61b4d18073b30
Wakana <wakanichi13@gmail.com>
2023-08-20 13:06:28 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let student = {
+  name: "George Machaidze",
+  age: 25,
+  grade: "A",
+};
+console.log(student);
+student.grade = "B";
+console.log(student);
+let car = {
+  brand: "Ford",
+  model: "Fusion",
+  year: 2013,
+};
+console.log("This is " + car.year + " " + car.brand + " " + car.model + " car");
+car.isSportCar = false;
+console.log(car);
